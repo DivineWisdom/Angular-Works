dd3016a03a3898f41090445f6b9162ee302fda8d
wisdomdivine <didiwiseman@gmail.com>
2020-04-22 13:45:04 +0100

    added more pages

---
+++ Binding/src/app/test/test.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+        <h2>
+          Welcome {{name}}
+        </h2>
+  `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+
+  public name = "Wisdom";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

652d614357002edf35f67427832b3851f73471af
wisdomdivine <didiwiseman@gmail.com>
2020-04-23 22:31:54 +0100

    Reactive forms

---
+++ Reactive-forms/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  get userName(){
+    return this.registrationForm.get('userName');
+  }
+
+  constructor(private fb: FormBuilder){}
+
+  registrationForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3)]],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city:[''],
+      state:[''],
+      postalCode: ['']
+    })
+  })
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Wisdom'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
+
+  loadApiData(){
+    this.registrationForm.setValue({
+      userName: 'Bruce',
+      password: 'test',
+      confirmPassword: 'test',
+      address:{
+        city: 'City',
+        state: 'State',
+        postalCode: '123456'
+      }
+    });
+  }
+
+}
